892b7d5f51f195b659e945f062678171072dce71
Voicu <bogdanvoicu115@gmail.com>
2024-12-19 19:34:41 +0200

    first commit

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import React from 'react'
+import{
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+    FormDescription
+}from "@/components/ui/form"
+import {Input} from '@/components/ui/input'
+import { Control } from 'react-hook-form'
+
+interface CustomProps{
+    control: Control<any>,
+}
+
+const CustomFormField = ({control}: CustomProps) => {
+  return (
+    <FormField
+          control={control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+  )
+}
+
+export default CustomFormField
