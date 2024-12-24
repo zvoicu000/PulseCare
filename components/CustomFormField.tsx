39422be5a307d3806210c80f166ccbfa6d940fa8
Voicu <bogdanvoicu115@gmail.com>
2024-12-24 15:55:14 +0200

    commit 6

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -17,7 +17,11 @@ import { FormFieldType } from './forms/PatientForm'
 import 'react-phone-number-input/style.css'
 import PhoneInput, {type Value} from 'react-phone-number-input'
 import { E164Number } from 'libphonenumber-js';
+import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
+import { Select, SelectContent, SelectTrigger, SelectValue } from './ui/select'
+import { Textarea } from './ui/textarea'
 
 
 interface CustomProps{
@@ -37,7 +41,7 @@ interface CustomProps{
 }
 
 const RenderField=({field,props}:{field:any;props:CustomProps})=>{
-  const {fieldType, iconSrc,iconAlt,placeholder} = props;
+  const {fieldType, iconSrc,iconAlt,placeholder, showTimeSelect,dateFormat, renderSkeleton} = props;
   switch (fieldType) {
     case FormFieldType.INPUT:
       return(
@@ -59,6 +63,17 @@ const RenderField=({field,props}:{field:any;props:CustomProps})=>{
             </FormControl>
         </div>
       )
+      case FormFieldType.TEXTAREA:
+        return (
+          <FormControl>
+            <Textarea
+            placeholder={placeholder}
+            {...field}
+            className='shad-textArea'
+            disabled={props.disabled}
+            />
+          </FormControl>
+        )
     case FormFieldType.PHONE_INPUT:
       return(
         <FormControl>
@@ -73,6 +88,46 @@ const RenderField=({field,props}:{field:any;props:CustomProps})=>{
           />
         </FormControl>
       )
+    case FormFieldType.DATE_PICKER:
+      return (
+        <div className='flex rounded-md border border-dark-500 bg-dark-400'>
+            <Image
+            src="/assets/icons/calendar.svg"
+            height={24}
+            width={24}
+            alt='calendar'
+            className='ml-2'
+            />
+            <FormControl>
+            <DatePicker selected={field.value} onChange={(date) => field.onChange(date)}
+            dateFormat={dateFormat ?? 'dd/MM/yyyy'}
+            showTimeSelect={showTimeSelect ?? false}
+            timeInputLabel='Time:'
+            wrapperClassName='date-picker' />
+            </FormControl>
+        </div>
+      )
+      case FormFieldType.SELECT:
+        return(
+          <FormControl>
+            <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl >
+                  <SelectTrigger className='shad-select-trigger'>
+                  <SelectValue placeholder={placeholder}/>
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent className='shad-select-content'>
+                    {props.children}
+                </SelectContent>
+            </Select>
+
+          </FormControl>
+        )
+      case FormFieldType.SKELETON:
+        return(
+          renderSkeleton ? renderSkeleton(field):null
+        )
+        
     default:
       break;
   }
--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -15,10 +15,11 @@ import { UserFormValidation } from "@/lib/validation"
 import { createUser } from "@/lib/actions/patient.actions"
 import { FormFieldType } from "./PatientForm"
 import { RadioGroup } from "@radix-ui/react-radio-group"
-import { GenderOptions } from "@/constants"
+import { Doctors, GenderOptions } from "@/constants"
 import { RadioGroupItem } from "../ui/radio-group"
 import { Label } from "../ui/label"
-
+import { SelectItem } from "../ui/select"
+import Image from "next/image"
 
 
 
@@ -129,18 +130,138 @@ const RegisterForm=({user}:{user:User}) =>{
         />
 
         </div>  
-        <div className="flex flex-col gap-6 xl:flex-row">
 
+        
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+        <CustomFormField
+        fieldType={FormFieldType.INPUT}
+        control={form.control}
+        name="adress"
+        label="Adress"
+        placeholder="14th Street, New York"
+        />
+
+        <CustomFormField
+        fieldType={FormFieldType.INPUT}
+        control={form.control}
+        name="occupation"
+        label="Occupation"
+        placeholder="Software Engineer"
+        />
 
         </div>  
         <div className="flex flex-col gap-6 xl:flex-row">
+        <CustomFormField
+        fieldType={FormFieldType.INPUT}
+        control={form.control}
+        name="emergencyContactName"
+        label="Emergency contact name"
+        placeholder="Guardian's name"
+        />
+        <CustomFormField
+        fieldType={FormFieldType.PHONE_INPUT}
+        control={form.control}
+        name="emergencyContactNumber"
+        label="Emergency contact number"
+        placeholder="(555) 123-4567"
+        
+        />
 
+        </div> 
 
-        </div>  
+          <section className="space-y-6">
+            <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Medical Information.</h2>
+            </div>
+        </section>
+
+        <CustomFormField
+        fieldType={FormFieldType.SELECT}
+        control={form.control}
+        name="primaryPhysician"
+        label="Primary Physician"
+        placeholder="Select a physician"
+        
+        >
+          {Doctors.map((doctor)=>(
+
+            <SelectItem key={doctor.name} value={doctor.name}>
+              <div className="flex cursor-pointer items-center gap-2">
+                  <Image src={doctor.image}
+                  width={32}
+                  height={32}
+                  alt={doctor.name}
+                  className="rounded-full border border-dark-500"
+                  />
+                  <p>{doctor.name}</p>
+              </div>
+            </SelectItem>
+          ))}
+
+        </CustomFormField>
+
         <div className="flex flex-col gap-6 xl:flex-row">
+        <CustomFormField
+        fieldType={FormFieldType.INPUT}
+        control={form.control}
+        name="insuranceProvider"
+        label="Insurance provider"
+        placeholder="BlueCross BlueShield"
+        />
 
+        <CustomFormField
+        fieldType={FormFieldType.INPUT}
+        control={form.control}
+        name="insurancePolicyNumber"
+        label="Insurance policy number"
+        placeholder="ABC123456789"
+        />
 
-        </div>  
+        </div>
+        <div className="flex flex-col gap-6 xl:flex-row">
+        <CustomFormField
+        fieldType={FormFieldType.TEXTAREA}
+        control={form.control}
+        name="allergies"
+        label="Allergies (if any)"
+        placeholder="Peanuts, Penicillin, Pollen"
+        />
+
+        <CustomFormField
+        fieldType={FormFieldType.TEXTAREA}
+        control={form.control}
+        name="currentMedication"
+        label="Current medication (if any)"
+        placeholder="Ibuprofen 200mg, Paracetamol 500mg"
+        />
+
+        </div>    
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+        <CustomFormField
+        fieldType={FormFieldType.TEXTAREA}
+        control={form.control}
+        name="familyMedicalHistory"
+        label="Family medical history"
+        placeholder="Mother had brain cancer, Father had heart disease"
+        />
+
+        <CustomFormField
+        fieldType={FormFieldType.TEXTAREA}
+        control={form.control}
+        name="pastMedicalHistory"
+        label="Past Medical History"
+        placeholder="Appendectomy, Tonsillectomy"
+        />
+
+        </div>   
+
+        <section className="space-y-6">
+            <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Identification and Verification</h2>
+            </div>
+        </section> 
 
         <SubmitButton isLoading={isLoading}>
           Get Started
@@ -152,4 +273,4 @@ const RegisterForm=({user}:{user:User}) =>{
 
 export default RegisterForm
 
-//1:37:00
+//2:00
